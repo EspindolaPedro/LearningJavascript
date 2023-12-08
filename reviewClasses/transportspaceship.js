142e1d42473b7833f312b1ec2584dee2c0684224
Pedro Paulo Espindola Nolasco <pedro.paulo.espindola2710@gmail.com>
2023-12-08 11:57:17 -0400

    Exercício de classes

---
+++ reviewClasses/transportspaceship.js
@@ -0,0 +1,6 @@
+class transportspaceship extends Spaceship {
+    constructor(name, crewQuantity, sitsQuantity) {
+        super(name, crewQuantity) 
+            this.sitsQuantity = sitsQuantity
+    }
+}
